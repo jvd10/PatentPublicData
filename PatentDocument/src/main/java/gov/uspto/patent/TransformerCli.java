77b85e3fff2112bf14753dec22b0eda72bb39834
Jay DePasse <depasse@psc.edu>
2020-02-19 13:37:01 -0500

    work through all exceptions

--- PatentDocument/src/main/java/gov/uspto/patent/TransformerCli.java
+++ PatentDocument/src/main/java/gov/uspto/patent/TransformerCli.java
@@ -14,6 +14,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Iterator;
+import java.lang.Exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -213,7 +214,9 @@ public class TransformerCli {
 					LOGGER.error("Patent Reader error: ", e1);
 				} catch (IOException e) {
 					LOGGER.error("Writer error: ", e);
-				}
+				} catch (Exception base_exception) {
+                    LOGGER.error("Unspecified exception: ", base_exception);
+                }
 			}
 
 		} finally {
